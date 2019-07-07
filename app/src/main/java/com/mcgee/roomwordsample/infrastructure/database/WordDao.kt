6fda22641ff13ecf22925afeac6465eb32965d00
Jennifer McGee <jennifer.mcgee@bottlerocketapps.com>
2019-07-06 21:05:40 -0500

    Added some to repo and dao as well as adding the viewmodel

--- app/src/main/java/com/mcgee/roomwordsample/infrastructure/database/WordDao.kt
+++ app/src/main/java/com/mcgee/roomwordsample/infrastructure/database/WordDao.kt
@@ -2,6 +2,7 @@ package com.mcgee.roomwordsample.infrastructure.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import com.mcgee.roomwordsample.model.Word
 
@@ -9,4 +10,10 @@ import com.mcgee.roomwordsample.model.Word
 interface WordDao {
     @Query("SELECT * from word_table ORDER BY word ASC")
     fun getAllWords(): LiveData<List<Word>>
+
+    @Insert
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    fun deleteAll()
 }
