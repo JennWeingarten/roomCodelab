1f1af18384b385e189fdd3f50426de15bcbe1b72
Jennifer McGee <jennifer.mcgee@bottlerocketapps.com>
2019-07-06 20:53:14 -0500

    Added to the dao and created a singleton in the for the database. Did it this was because I am following along in codelab. Should really be in di pattern.

--- app/src/main/java/com/mcgee/roomwordsample/infrastructure/database/WordDao.kt
+++ app/src/main/java/com/mcgee/roomwordsample/infrastructure/database/WordDao.kt
@@ -1,4 +1,12 @@
 package com.mcgee.roomwordsample.infrastructure.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+import com.mcgee.roomwordsample.model.Word
+
+@Dao
 interface WordDao {
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAllWords(): LiveData<List<Word>>
 }
